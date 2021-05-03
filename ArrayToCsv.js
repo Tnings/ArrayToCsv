8b4b95db24a0dd4f825e1bc6305aceaff6fe85a0
Tnings <55812511+Tnings@users.noreply.github.com>
2021-05-03 12:29:24 -0700

    1.0 Release
    Basic functionality, load based conversion to allow massive arrays to be processed without crashing.

---
+++ ArrayToCsv.js
@@ -0,0 +1,47 @@
+let fs = require('fs');
+const p = require("path");
+
+function process(array, path, loadSize, installPath, options){
+    let fileName = p.basename(path,p.extname(path));
+    let tempString = '';
+    let numOfLoads = parseInt(array.length / loadSize);
+    let currentLoadNum = 0;
+    let install = `${installPath}/${fileName}.csv`;
+
+    for(let m = 0; m <= numOfLoads; m++) {
+        if(options.logs) {
+            console.clear();
+            console.log(`Load ${m} out of ${numOfLoads}`);
+        }
+
+        for (let i = 0+(currentLoadNum*loadSize); i <= parseInt((array.length/numOfLoads)+(currentLoadNum*loadSize)); i++) {
+            if(array[i]){
+                tempString = tempString + array[i]+'\n';
+            }
+        }
+        currentLoadNum++;
+        if(m === 0){
+            fs.writeFile(install, tempString, err => {
+                if (err) {
+                    console.error(err)
+                }
+            })
+        }
+        else{
+            fs.appendFile(install,tempString,err => {
+                if (err) {
+                    console.error(err)
+                }
+            });
+        }
+        tempString = 0;
+    }
+
+    if(options.logs) {
+        console.clear();
+        console.log(`File created at ${installPath}`);
+    }
+
+}
+
+module.exports = { process };
